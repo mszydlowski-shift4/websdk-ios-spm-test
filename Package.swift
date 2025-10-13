8242e4af1f627a00d6b10e416208c88d5ff670d9
Marcin Szydłowski <marcin.szydlowski@codahead.com>
2025-10-13 10:38:20 +0200

    Add SPM wrapper for VNWebSDK v3.2.1

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "VNWebSDK",
+	platforms: [
+		.iOS(.v12)
+	],
+	products: [
+		.library(
+			name: "VNWebSDK",
+			targets: ["VNWebSDK"]
+		)
+	],
+	targets: [
+		.binaryTarget(
+			name: "VNWebSDK",
+			url: "https://venuenext.jfrog.io/artifactory/ios-sdk/ios-sdk-v3.2.1-xcframework-xcode14.2.0.zip",
+			checksum: "3bf50f3a05d823a958b36076da596cbb504523fa31452f21bead011b04f84f60"
+		)
+	]
+)
